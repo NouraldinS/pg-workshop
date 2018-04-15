1368929fabd7bc7466e403c1039b5c4d926fa320
= <noureldean.saed@gmail.com>
2018-04-15 18:23:13 +0300

    Minor edits, returned the error that was supposed to happen, and changed the 3000 port to 4000, what can I say, I just love 4000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const handler = require('./src/handler.js');
 
 const server = http.createServer(handler);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
   console.log(`Magic happens on port ${port}`);
